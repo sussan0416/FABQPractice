33a621698af176a1af13da381ccd50fcc27d346a
sussan0416 <sussan.0416@gmail.com>
2019-08-20 23:01:14 +0900

    add: create FABQPractice project

---
+++ FABQPractice/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  FABQPractice
+//
+//  The aim of this ViewController is to check how Firebase SDK perform
+//  when setScreenName is called, setUserID is called.
+//
+//  Created by 鈴木孝宏 on 2019/08/20.
+//  Copyright © 2019 鈴木孝宏. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UITableViewController {
+
+    @IBOutlet weak var userIdTextField: UITextField!
+    
+    @IBOutlet weak var screenNameTextField: UITextField!
+    
+    private let defaultScreenName = "first screen"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Analytics.setScreenName(defaultScreenName, screenClass: nil)
+    }
+    
+    @IBAction func tapUserIDSendButton(_ sender: Any) {
+        guard let userID = userIdTextField.text, !userID.isEmpty else {
+            showAlert(with: "userID is set to nil")
+            Analytics.setUserID(nil)
+            return
+        }
+        
+        Analytics.setUserID(userID)
+    }
+    
+    @IBAction func tapScreenNameSendButton(_ sender: Any) {
+        guard let screenName = screenNameTextField.text, !screenName.isEmpty else {
+            showAlert(with: "screenName is set to nil")
+            return
+        }
+        
+        Analytics.setScreenName(screenName, screenClass: nil)
+    }
+    
+    private func showAlert(with message: String) {
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
